0f577aed1b0051b37ce3bfff4c5d0d0f5eef4ec2
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2020-05-18 10:50:36 +0100

    updated install.sh

--- install.sh
+++ install.sh
@@ -3,42 +3,37 @@
 # location of dotfiles repo
 REPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # symlink to dotfiles repo
-DOTFILES="/home/martin/.dotfiles"
+DOTFILES="$HOME/.dotfiles"
 
-
-link () {	
-	echo "This script will symlink the files in this repo to their corresponding locations on the system."
-	echo "Proceed? (Y/n)"
-	read resp
-	if [ "$resp" = 'y' -o "$resp" = 'Y' -o "$resp" = '' ]; then
-		# if symlink to dotfiles doesn't exist, create it
-		if [ ! -L "$DOTFILES" ]; then
-			ln -sf $REPO_DIR $DOTFILES
-		fi
-
-		# ~/.bashrc
-		ln -sf $DOTFILES/bashrc ~/.bashrc
-
-		# ~/.bash_aliases
-		ln -sf $DOTFILES/bash_aliases ~/.bash_aliases
-
-		# ~/.gitconfig
-		ln -sf $DOTFILES/gitconfig ~/.gitconfig
-
-		# ~/.gitignore_global
-		ln -sf $DOTFILES/gitignore_global ~/.gitignore_global
-
-		# libinput-gestures.conf
-		ln -sf $DOTFILES/config/libinput-gestures.conf ~/.config/libinput-gestures.conf
-
-		# bundled.desktop
-		ln -sf $DOTFILES/applications/bundled.desktop ~/.local/share/applications/bundled.desktop
-
-		echo "Symlinks created."
-	else 
-		echo "Cancelled."
+	
+echo "This script will symlink the files in this repo to their corresponding locations on the system."
+echo "Proceed? (Y/n)"
+read resp
+if [ "$resp" = 'y' -o "$resp" = 'Y' -o "$resp" = '' ]; then
+	# if symlink to dotfiles doesn't exist, create it
+	if [ ! -L "$DOTFILES" ]; then
+		ln -sf $REPO_DIR $DOTFILES
 	fi
-}
 
+	# ~/.bashrc
+	ln -sf $DOTFILES/bashrc ~/.bashrc
 
-link
+	# ~/.bash_aliases
+	ln -sf $DOTFILES/bash_aliases ~/.bash_aliases
+
+	# ~/.gitconfig
+	ln -sf $DOTFILES/gitconfig ~/.gitconfig
+
+	# ~/.gitignore_global
+	ln -sf $DOTFILES/gitignore_global ~/.gitignore_global
+
+	# libinput-gestures.conf
+	ln -sf $DOTFILES/config/libinput-gestures.conf ~/.config/libinput-gestures.conf
+
+	# bundled.desktop
+	ln -sf $DOTFILES/applications/bundled.desktop ~/.local/share/applications/bundled.desktop
+
+	echo "Symlinks created."
+else 
+	echo "Cancelled."
+fi
