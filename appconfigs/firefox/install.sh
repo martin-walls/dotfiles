e43df40b5f4d3c6a763c3d884eb077b2d5bd2a4a
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2020-05-22 12:07:07 +0100

    firefox new tab page config

---
+++ appconfigs/firefox/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DOTFILES="$HOME/.dotfiles"
+
+echo "This script will symlink the files in this repo to their corresponding locations on the system."
+echo "Proceed? (Y/n)"
+read resp
+if [ "$resp" = 'y' -o "$resp" = 'Y' -o "$resp" = '' ]; then
+	sudo ln -svf $DOTFILES/appconfigs/firefox/autoconfig.js /usr/lib/firefox/defaults/pref/autoconfig.js
+
+	sudo ln -svf $DOTFILES/appconfigs/firefox/firefox.cfg /usr/lib/firefox/firefox.cfg
+
+	echo "Symlinks created."
+else 
+	echo "Cancelled."
+fi
