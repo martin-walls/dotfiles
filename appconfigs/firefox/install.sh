e7eb7a0f9d32efbc592f24c936c1d50923e345a0
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2020-10-12 14:23:45 +0100

    updated file path, added README

--- appconfigs/firefox/install.sh
+++ appconfigs/firefox/install.sh
@@ -6,7 +6,7 @@ echo "This script will symlink the files in this repo to their corresponding loc
 echo "Proceed? (Y/n)"
 read resp
 if [ "$resp" = 'y' -o "$resp" = 'Y' -o "$resp" = '' ]; then
-	sudo ln -svf $DOTFILES/appconfigs/firefox/autoconfig.js /usr/lib/firefox/defaults/pref/autoconfig.js
+	sudo ln -svf $DOTFILES/appconfigs/firefox/autoconfig.js /usr/lib/firefox/defaults/pref/local-settings.js
 
 	sudo ln -svf $DOTFILES/appconfigs/firefox/firefox.cfg /usr/lib/firefox/firefox.cfg
 
