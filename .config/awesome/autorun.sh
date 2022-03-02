f70838ea35b33e2b7d4d6f5549370870b1b7e385
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2022-03-02 22:31:22 +0000

    initial awesome config

---
+++ .config/awesome/autorun.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+~/.config/screenlayout/main.sh
---
+++ .config/screenlayout/main.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP --primary --mode 2560x1600 --pos 2560x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --mode 2560x1440 --pos 0x80 --rotate normal
