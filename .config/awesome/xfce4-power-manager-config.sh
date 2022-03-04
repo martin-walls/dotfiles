ae18257c64bfcfc2b0b3ab094e822462d7c855bf
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2022-03-04 22:05:31 +0000

    added comment

--- .config/awesome/xfce4-power-manager-config.sh
+++ .config/awesome/xfce4-power-manager-config.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# for some reason this doesn't seem to persist, so set it every time awesome starts
 xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/logind-handle-lid-switch -s false
