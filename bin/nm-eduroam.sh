78cad552ad768dad71a3dbf172c4f972afe26a9e
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2022-03-13 22:41:22 +0000

    network manager eduroam setup script

---
+++ bin/nm-eduroam.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [[ $# != 3 ]]; then
+	echo "Usage: nm-eduroam.sh <username> <password> <cert>"
+fi
+
+username=$1
+password=$2
+cert=$3
+
+echo 'Adding new connection "eduroam"...'
+
+nmcli con add \
+	type wifi \
+	con-name "eduroam" \
+	ifname "wlp1s0" \
+	ssid "eduroam" \
+	wifi-sec.key-mgmt "wpa-eap" \
+	wifi-sec.pairwise "ccmp" \
+	wifi-sec.group "ccmp,tkip" \
+	802-1x.eap "peap" \
+	802-1x.ca-cert "$cert" \
+	802-1x.identity "$username" \
+	802-1x.password "$password" \
+	802-1x.altsubject-matches "DNS:token.wireless.cam.ac.uk" \
+	802-1x.phase2-auth "mschapv2" \
+	802-1x.anonymous-identity "_token@cam.ac.uk"
+
+echo 'Connection added.'
+echo 'Activating connection "eduroam"...'
+
+nmcli connection up eduroam
+
+echo 'Connection activated.'
