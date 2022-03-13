0d5798e7cdeaf7767d52e3478077f01a93475b7b
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2022-03-13 21:29:16 +0000

    spotify module

---
+++ .config/waybar/modules/spotify.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+playing=$(playerctl metadata --player=spotify --format '{{lc(status)}}')
+icon="阮"
+fulltrackinfo=""
+
+if [[ $playing == "playing" ]]; then
+  trackinfo=$(playerctl metadata --player=spotify --format '{{title}} - {{artist}}')
+  fulltrackinfo=$trackinfo
+  if [[ ${#trackinfo} > 25 ]]; then
+    trackinfo=$(echo $trackinfo | cut -c1-22)"..."
+  fi
+  text=$icon" "$trackinfo
+elif [[ $playing == "paused" ]]; then
+  text=$icon
+elif [[ $playing == "stopped" ]]; then
+  text=""
+fi
+
+echo -e "{\"text\":\""$text"\", \"class\":\""$playing"\", \"tooltip\":\""$fulltrackinfo"\"}"
