680a903e4f06492c663b6306a0cad509122d5203
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2022-03-13 19:49:07 +0000

    more sway config
    - blurred lockscreen
    - wofi launcher

---
+++ .config/swaylock/lock.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+blueblack="282c34"
+white="abb2bf"
+
+swaylock \
+  --screenshots \
+  --effect-scale 0.25 \
+  --effect-blur 15x2 \
+  --effect-scale 4 \
+  --clock \
+  --timestr "%H:%M" \
+  --datestr "%a %d %b" \
+  --indicator \
+  --font "JetBrainsMono Nerd Font Mono" \
+  --inside-color $blueblack \
+  --text-color $white \
+  --ignore-empty-password
