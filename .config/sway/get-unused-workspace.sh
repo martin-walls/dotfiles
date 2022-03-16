4398848279556ca0d0286711d8bcde1862ccd3b4
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2022-03-16 12:36:55 +0000

    better comments

--- .config/sway/get-unused-workspace.sh
+++ .config/sway/get-unused-workspace.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Note: workspaces must be added to the following lists in
+# ascending number order, cos the script assumes sorted order
+# for efficiency. All workspaces should be specified.
+#
 # workspace names to use as general-purpose workspaces
 wksps_use=(
   '1'
@@ -23,6 +27,8 @@ wksps_use=(
   '21'
   '22'
 )
+# workspace names to never use as new workspace, even if
+# they're empty
 wksps_ignore=(
   '11:阮'
   '12:K'
@@ -31,11 +37,13 @@ wksps_ignore=(
 i=0
 j=0
 
+# skip workspaces currently in use
 for w in $(swaymsg -t get_workspaces | jq -r 'sort_by(.name[0:2]|tonumber)|.[].name'); do
   if [[ ${wksps_ignore[j]} == $w ]]; then
     j=$(($j+1))
     continue
   fi
+  # return the first workspace not in use
   if [[ "${wksps_use[i]}" != $w ]]; then
     break
   fi
