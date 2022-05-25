e9f7a00c881fe6f361e996617cca8114990fbddb
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2022-05-25 11:33:56 +0100

    new version of new workspace finder script, to work with sworkstyle

--- sway.config/scripts/get-unused-workspace.sh
+++ sway.config/scripts/get-unused-workspace.sh
@@ -1,53 +1,17 @@
 #!/bin/bash
 
-# Note: workspaces must be added to the following lists in
-# ascending number order, cos the script assumes sorted order
-# for efficiency. All workspaces should be specified.
-#
-# workspace names to use as general-purpose workspaces
-wksps_use=(
-  '1'
-  '2'
-  '3'
-  '4'
-  '5'
-  '6'
-  '7'
-  '8'
-  '9'
-  '10'
-  '11'
-  '12'
-  '13'
-  '14'
-  '15'
-  '16'
-  '17'
-  '18'
-  '19'
-  '20'
-  '21'
-  '22'
-)
-# workspace names to never use as new workspace, even if
-# they're empty
-wksps_ignore=(
-)
+# New version to work with sworkstyle's dynamic workspace names.
+# Works with workspace numbers rather than names.
 
-i=0
-j=0
+i=1
 
-# skip workspaces currently in use
-for w in $(swaymsg -t get_workspaces | jq -r 'sort_by(.name[0:2]|tonumber)|.[].name'); do
-  if [[ ${wksps_ignore[j]} == $w ]]; then
-    j=$(($j+1))
-    continue
-  fi
-  # return the first workspace not in use
-  if [[ "${wksps_use[i]}" != $w ]]; then
+for w in $(swaymsg -t get_tree | jq '.nodes[].nodes[] | select(.type == "workspace") | select(has("num")) | .num'); do
+  if [ $i != $w ]; then
+    # found a gap in consecutive workspaces
+    # so use the number we got to as the new workspace
     break
   fi
   i=$(($i+1))
 done
 
-echo "${wksps_use[i]}"
+echo $i
