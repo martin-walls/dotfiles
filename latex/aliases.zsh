677eba4d7de87037ae36513a27d10b6343fbfcdc
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2022-10-05 13:14:54 +0100

    add latex compile aliases

---
+++ latex/aliases.zsh
@@ -0,0 +1,5 @@
+#  ┌───────────────┐
+#  │ LaTeX Aliases │
+#  └───────────────┘
+alias latexcompile='latexmk -xelatex -shell-escape -verbose -file-line-error -interaction=nonstopmode -pvc -pvctimeout'
+alias latexclean='latexmk -c'
