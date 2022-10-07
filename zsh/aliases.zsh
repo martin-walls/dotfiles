98f9823ffcaff54363692e835252e2a912a89acc
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2022-10-07 19:57:01 +0100

    add screenshot to file function

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,3 +5,12 @@ alias rmdir='rmdirtrash'
 
 # allow using sudo with aliases
 alias sudo='sudo '
+
+# paste screenshot to file
+clip2png () {
+  filename="$(date +%Y.%m.%d.%T).png"
+  if [ $# -ge 1 ]; then
+    filename=$1
+  fi
+  wl-paste -t image/png > $filename
+}
