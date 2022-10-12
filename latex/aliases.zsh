eff7f13656892e03548d1405cf49e4c281f0deba
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2022-10-12 11:56:37 +0100

    add wordcount alias

--- latex/aliases.zsh
+++ latex/aliases.zsh
@@ -3,3 +3,4 @@
 #  └───────────────┘
 alias latexcompile='latexmk -xelatex -shell-escape -verbose -file-line-error -interaction=nonstopmode -pvc -pvctimeout'
 alias latexclean='latexmk -c'
+alias latexwc='texcount -inc -total -sum'
