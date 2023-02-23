dabcb2d2dbde299ca8fa6a7c94c7e9631c5dcd28
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2023-02-23 15:20:42 +0000

    Update git log format

--- git/plugin.zsh
+++ git/plugin.zsh
@@ -2,6 +2,7 @@
 
 # disable default aliases; added the aliases I use in ./aliases.zsh
 export FORGIT_NO_ALIASES=1
+export FORGIT_LOG_FORMAT="%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset"
 # add forgit to path so I can manually set aliases
 export PATH="$PATH:$HOME/.dotfiles/git/forgit/bin"
 source ~/.dotfiles/git/forgit/forgit.plugin.zsh
