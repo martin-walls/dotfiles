7102c6efadbb215298963c051ae8b552ff91a88b
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2023-02-25 21:02:43 +0000

    Go back to kitty git diff

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -5,12 +5,12 @@
 alias gs='git st'
 alias gc='git cm'
 alias gp='git push'
-# alias gd='git df'
+alias gd='git df'
 alias gpl='git pull'
 alias gf='git fetch'
 # alias gg='git lgb'
 
 # Use forgit plugin for these
-alias gd='git forgit diff'
+# alias gd='git forgit diff'
 alias ga='git forgit add'
 alias gg='git forgit log'
