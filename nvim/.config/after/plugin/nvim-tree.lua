d290d032af2820536cc10361fdc2187161ad9631
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2023-03-13 12:11:32 +0000

    Open nvim-tree if we open nvim to a folder

--- nvim/.config/after/plugin/nvim-tree.lua
+++ nvim/.config/after/plugin/nvim-tree.lua
@@ -11,6 +11,27 @@ require("nvim-tree").setup {
         -- show files that are gitignored
         ignore = false,
     },
+    reload_on_bufenter = true,
 }
 
 vim.keymap.set("n", "<leader>-", vim.cmd.NvimTreeToggle)
+
+-- Open nvim-tree if we open nvim to a folder
+vim.api.nvim_create_autocmd({ "VimEnter" },
+    {
+        callback = function (data)
+            -- buffer is a directory
+            local is_directory = vim.fn.isdirectory(data.file) == 1
+
+            if not is_directory then
+                return
+            end
+
+            -- change to the directory
+            vim.cmd.cd(data.file)
+
+            -- open the tree
+            require("nvim-tree.api").tree.open()
+        end
+    }
+)
