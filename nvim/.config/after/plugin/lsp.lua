1b6538179b025404f5a9c28203f1105fd15a28eb
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2023-03-13 17:27:12 +0000

    LSP diagnostics config

--- nvim/.config/after/plugin/lsp.lua
+++ nvim/.config/after/plugin/lsp.lua
@@ -1,6 +1,6 @@
 local lsp = require('lsp-zero').preset({
   name = 'minimal',
-  set_lsp_keymaps = true,
+  set_lsp_keymaps = {omit = {"gl"}},
   manage_nvim_cmp = true,
   suggest_lsp_servers = true,
 })
@@ -11,7 +11,20 @@ lsp.ensure_installed({
 	"eslint",
 })
 
+-- Keybindings
+lsp.on_attach(function (client, bufnr)
+    local opts = {buffer = bufnr}
+
+    vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+end)
+
 -- (Optional) Configure lua language server for neovim
 lsp.nvim_workspace()
 
 lsp.setup()
+
+vim.diagnostic.config({
+    virtual_text = true,
+    update_in_insert = true,
+})
+
