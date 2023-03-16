125c63bf4152851708e7c26d7611d1c0eca5c722
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2023-03-16 15:33:57 +0000

    More harpoon shortcuts

--- nvim/.config/after/plugin/harpoon.lua
+++ nvim/.config/after/plugin/harpoon.lua
@@ -2,10 +2,15 @@ local ui = require("harpoon.ui")
 local mark = require("harpoon.mark")
 
 vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+vim.keymap.set("n", "<A-e>", ui.toggle_quick_menu)
 
 -- Quickly jump to files
 vim.keymap.set("n", "<A-1>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<A-2>", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<A-3>", function() ui.nav_file(3) end)
 vim.keymap.set("n", "<A-4>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<A-5>", function() ui.nav_file(5) end)
+
+-- Cycle through harpooned files
+vim.keymap.set("n", "<A-q>", ui.nav_next)
+vim.keymap.set("n", "<A-w>", ui.nav_prev)
