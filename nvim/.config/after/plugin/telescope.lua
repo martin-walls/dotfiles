abdba19e02565963f2e03dde5153de29811c40a5
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2023-03-17 10:59:45 +0000

    Tweak telescope mappings

--- nvim/.config/after/plugin/telescope.lua
+++ nvim/.config/after/plugin/telescope.lua
@@ -1,6 +1,6 @@
 local builtin = require('telescope.builtin')
 -- Search all files in project
-vim.keymap.set('n', '<leader>pf', builtin.find_files)
+vim.keymap.set('n', '<leader>pv', builtin.find_files)
 -- Search all files in the git repo
 vim.keymap.set('n', '<C-p>', builtin.git_files)
 -- Grep for a search string across project
@@ -14,7 +14,7 @@ vim.keymap.set("n", "<leader>pb", builtin.buffers)
 -- All diagnostics
 vim.keymap.set("n", "<leader>pd", builtin.diagnostics)
 -- Current file diagnostics
-vim.keymap.set("n", "<leader>pD", function()
+vim.keymap.set("n", "<leader>pf", function()
     builtin.diagnostics({ bufnr = 0 })
 end)
 -- References of symbol under cursor
