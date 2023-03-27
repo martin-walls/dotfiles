57355725f67ab332d91aa590b6e5618c0e813101
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2023-03-27 18:57:19 +0100

    ChkTeX: Don't execute \input commands

--- nvim/.config/after/plugin/null-ls.lua
+++ nvim/.config/after/plugin/null-ls.lua
@@ -5,6 +5,11 @@ null_ls.setup({
         null_ls.builtins.diagnostics.eslint_d,
         null_ls.builtins.formatting.prettierd,
         null_ls.builtins.diagnostics.chktex.with({
+            -- Don't execute \input statements -- it was showing diagnostics for
+            -- included files at the line numbers of that file in the current file...
+            -- rather confusing and hard to find
+            extra_args = { "--inputfiles=0" },
+            -- Show error code as well as message, so it can be silenced via a line comment
             diagnostics_format = "[#{c}] #{m}",
         }),
     },
