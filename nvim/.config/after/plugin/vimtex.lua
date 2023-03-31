52d7b14861e3a6a68a6c716cb61dc1cc13c884c9
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2023-03-31 22:27:47 +0100

    VimTeX: recognise EnumerateAlgorithm as a list environment
    So that \items are folded properly ie. constrained to within the list
    environment.

--- nvim/.config/after/plugin/vimtex.lua
+++ nvim/.config/after/plugin/vimtex.lua
@@ -4,18 +4,18 @@ vim.g.tex_conceal = "abdg"
 vim.opt.conceallevel = 1
 -- Disable conceals that are hard to read
 vim.g.vimtex_syntax_conceal = {
-      accents = 1,
-      cites = 1,
-      fancy = 1,
-      greek = 1,
-      math_bounds = 1,
-      math_delimiters = 1,
-      math_fracs = 0,
-      math_super_sub = 0,
-      math_symbols = 1,
-      sections = 0,
-      styles = 1,
-      ligatures = 1,
+    accents = 1,
+    cites = 1,
+    fancy = 1,
+    greek = 1,
+    math_bounds = 1,
+    math_delimiters = 1,
+    math_fracs = 0,
+    math_super_sub = 0,
+    math_symbols = 1,
+    sections = 0,
+    styles = 1,
+    ligatures = 1,
 }
 
 -- Disable insert mode mappings
@@ -35,3 +35,12 @@ vim.g.vimtex_fold_types = {
     }
 }
 
+vim.g.vimtex_indent_lists = {
+    -- Defaults from the docs
+    "itemize",
+    "enumerate",
+    "description",
+    "thebibliography",
+    -- For dissertation
+    "EnumerateAlgorithm",
+}
