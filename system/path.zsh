4e63503a32993befdb96da30f6f910a42538679b
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2023-06-15 17:11:09 +0100

    Add go to system PATH

--- system/path.zsh
+++ system/path.zsh
@@ -1,1 +1,1 @@
-export PATH="$HOME/.dotfiles/scripts:$HOME/.local/bin:$HOME/.local/bin/booky:$PATH"
+export PATH="$HOME/.dotfiles/scripts:$HOME/.local/bin:$HOME/.local/bin/booky:/usr/local/go/bin:$PATH"
