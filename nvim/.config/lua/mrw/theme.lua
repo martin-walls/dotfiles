633b20dc278f1e795d30c85a03411f46874d2eeb
mrw24 <mrw24@lyrane.cs.st-andrews.ac.uk>
2023-09-09 13:48:24 +0100

    Remove system theme checking

--- nvim/.config/lua/mrw/theme.lua
+++ nvim/.config/lua/mrw/theme.lua
@@ -5,17 +5,17 @@ require("onedark").setup({
 })
 require("onedark").load()
 
-local function get_system_theme()
-    local theme_file = io.open(os.getenv("HOME") .. "/.dotfiles/theme/CURRENT_THEME", "r")
-    if (theme_file == nil) then
-        return "dark"
-    end
-    return theme_file:read("*l")
-end
-
-local system_theme = get_system_theme()
-if system_theme == "light" then
-    vim.o.background = "light"
-else
-    vim.o.background = "dark"
-end
+-- local function get_system_theme()
+--     local theme_file = io.open(os.getenv("HOME") .. "/.dotfiles/theme/CURRENT_THEME", "r")
+--     if (theme_file == nil) then
+--         return "dark"
+--     end
+--     return theme_file:read("*l")
+-- end
+--
+-- local system_theme = get_system_theme()
+-- if system_theme == "light" then
+--     vim.o.background = "light"
+-- else
+--     vim.o.background = "dark"
+-- end
