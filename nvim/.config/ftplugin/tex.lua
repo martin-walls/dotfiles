54d37b8f650c4473c049d387a4ea2a216e42c16c
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2023-09-13 10:40:34 +0100

    Create a document template for STA work

--- nvim/.config/ftplugin/tex.lua
+++ nvim/.config/ftplugin/tex.lua
@@ -14,28 +14,32 @@ vim.opt_local.spell = true
 -- AutoSave tex files
 local autoSaveGroup = vim.api.nvim_create_augroup("TexAutoSaveOnWrite", { clear = true })
 vim.api.nvim_create_autocmd({ "TextChangedI", "TextChanged", "InsertLeave", "BufLeave" }, {
-    pattern = { "*.tex", "*.bib" },
-    group = autoSaveGroup,
-    callback = function(args)
-        local buf = args.buf
-        -- Check that buffer isn't readonly and that the file is readable
-        if vim.fn.getbufvar(buf, "&modifiable") == 1 and
-            vim.fn.filereadable(vim.api.nvim_buf_get_name(buf)) then
-            vim.cmd("silent! write")
-        end
+  pattern = { "*.tex", "*.bib" },
+  group = autoSaveGroup,
+  callback = function(args)
+    local buf = args.buf
+    -- Check that buffer isn't readonly and that the file is readable
+    if vim.fn.getbufvar(buf, "&modifiable") == 1 and
+        vim.fn.filereadable(vim.api.nvim_buf_get_name(buf)) then
+      vim.cmd("silent! write")
     end
+  end
 })
 
 -- Global function to insert supo work template into file
 function SupoWorkTemplate()
-    vim.cmd("0r " .. vim.fn.stdpath('config') .. "/templates/supotemplate.tex")
+  vim.cmd("0r " .. vim.fn.stdpath('config') .. "/templates/supotemplate.tex")
+end
+
+function StaWorkTemplate()
+  vim.cmd("0r " .. vim.fn.stdpath('config') .. "/templates/statemplate.tex")
 end
 
 -- Clear indentexpr when open TeX file -- this disables auto indenting
 vim.api.nvim_create_autocmd("BufEnter", {
-    pattern = { "*.tex", "*.bib" },
-    group = vim.api.nvim_create_augroup("TexClearIndentExpr", { clear = true }),
-    callback = function()
-        vim.opt.indentexpr = ""
-    end,
+  pattern = { "*.tex", "*.bib" },
+  group = vim.api.nvim_create_augroup("TexClearIndentExpr", { clear = true }),
+  callback = function()
+    vim.opt.indentexpr = ""
+  end,
 })
