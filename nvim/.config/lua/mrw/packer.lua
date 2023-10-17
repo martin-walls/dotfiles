94d809633d0a4123c9be2481969936dac8487efa
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2023-10-17 15:45:22 +0100

    nvim-treesitter-textobjects plugin

--- nvim/.config/lua/mrw/packer.lua
+++ nvim/.config/lua/mrw/packer.lua
@@ -13,6 +13,11 @@ return require('packer').startup(function(use)
     }
     use("navarasu/onedark.nvim")
     use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+    use({
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        after = "nvim-treesitter",
+        requires = "nvim-treesitter/nvim-treesitter",
+    })
     use("nvim-treesitter/playground")
     use("nvim-treesitter/nvim-treesitter-context")
     -- use("mbbill/undotree")
