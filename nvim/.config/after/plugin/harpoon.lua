dfe89eb06064e4904701d8dee8b9670692b47fd5
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2024-02-29 11:26:58 +0000

    harpoon keybind changed

--- nvim/.config/after/plugin/harpoon.lua
+++ nvim/.config/after/plugin/harpoon.lua
@@ -1,7 +1,7 @@
 local ui = require("harpoon.ui")
 local mark = require("harpoon.mark")
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<A-a>", mark.add_file)
 vim.keymap.set("n", "<A-e>", ui.toggle_quick_menu)
 
 -- Quickly jump to files
