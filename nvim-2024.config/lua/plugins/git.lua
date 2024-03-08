f69e635e74159f08a56ee949b7d4e2ec1e6a37a5
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2024-03-08 20:55:33 +0000

    v2024: fugitive

---
+++ nvim-2024.config/lua/plugins/git.lua
@@ -0,0 +1,11 @@
+return {
+    "tpope/vim-fugitive",
+    config = function()
+        vim.keymap.set("n", "<leader>gs", function()
+            vim.cmd("Git")
+        end, {desc = "[G]it [S]tatus"})
+        vim.keymap.set("n", "<leader>gp", function()
+            vim.cmd("Git push")
+        end, {desc = "[G]it [P]ush"})
+    end,
+}
--- nvim-2024.config/lua/plugins/which-key.lua
+++ nvim-2024.config/lua/plugins/which-key.lua
@@ -1,17 +1,17 @@
 return {
     "folke/which-key.nvim",
     event = "VimEnter", -- When to load the plugin
-    config = function ()
+    config = function()
         require("which-key").setup()
 
         -- Document existing key chains
-        require('which-key').register({
+        require("which-key").register({
             ["<leader>p"] = { name = "[P]ick", _ = "which_key_ignore" },
-            ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-            ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-            ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-            -- ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-            -- ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+            ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
+            ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+            ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
+            ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
+            ["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
         })
-    end
+    end,
 }
