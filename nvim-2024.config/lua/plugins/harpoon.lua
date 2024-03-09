b2af011919d59b46c4cea17c2d6b79867c90b1dc
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2024-03-09 13:14:03 +0000

    v2024: harpoon

---
+++ nvim-2024.config/lua/plugins/harpoon.lua
@@ -0,0 +1,57 @@
+return {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    config = function()
+        local harpoon = require("harpoon")
+        harpoon.setup({
+            settings = {
+                save_on_toggle = true,
+                save_on_ui_close = true,
+            },
+        })
+
+        vim.keymap.set("n", "<leader>ha", function()
+            harpoon:list():append()
+        end, { desc = "Harpoon Add" })
+
+        vim.keymap.set("n", "<leader>hl", function()
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+        end, { desc = "Harpoon List" })
+
+        vim.keymap.set("n", "<leader>hc", function()
+            harpoon:list():clear()
+        end, { desc = "Harpoon Clear" })
+
+        for i = 1, 9 do
+            vim.keymap.set("n", "<leader>h" .. i, function()
+                harpoon:list():select(i)
+            end, { desc = "Jump to harpoon file [" .. i .. "]" })
+        end
+
+        local conf = require("telescope.config").values
+        local function toggle_telescope(harpoon_files)
+            local file_paths = {}
+            for _, item in ipairs(harpoon_files.items) do
+                table.insert(file_paths, item.value)
+            end
+
+            require("telescope.pickers")
+                .new({}, {
+                    prompt_title = "Harpoon",
+                    finder = require("telescope.finders").new_table({
+                        results = file_paths,
+                    }),
+                    previewer = conf.file_previewer({}),
+                    sorter = conf.generic_sorter({}),
+                })
+                :find()
+        end
+
+        vim.keymap.set("n", "<leader>ht", function()
+            toggle_telescope(harpoon:list())
+        end, { desc = "Harpoon Telescope" })
+    end,
+}
--- nvim-2024.config/lua/plugins/which-key.lua
+++ nvim-2024.config/lua/plugins/which-key.lua
@@ -12,6 +12,7 @@ return {
             ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
             ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
             ["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
+            ["<leader>h"] = { name = "[H]arpoon", _ = "which_key_ignore" },
         })
     end,
 }
