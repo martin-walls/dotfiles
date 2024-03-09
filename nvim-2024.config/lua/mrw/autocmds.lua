4ff1a5a73e6e715dcae802162aefd991dd81a049
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2024-03-09 16:23:52 +0000

    v2024: autocmds

--- nvim-2024.config/lua/mrw/autocmds.lua
+++ nvim-2024.config/lua/mrw/autocmds.lua
@@ -4,5 +4,46 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     group = vim.api.nvim_create_augroup("HighlightAfterYank", { clear = true }),
     callback = function()
         vim.highlight.on_yank()
-    end
+    end,
+})
+
+-- Auto-save files when leaving a buffer
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+    group = vim.api.nvim_create_augroup("SetUpBufferAutoSave", { clear = true }),
+    callback = function(args)
+        local bufnr = args.buf
+        -- Create buffer-local autocmd to autosave
+        vim.api.nvim_create_autocmd({ "BufLeave" }, {
+            group = vim.api.nvim_create_augroup("AutoSaveBuffer", { clear = true }),
+            buffer = bufnr,
+            callback = function()
+                -- Check that buffer isn't readonly and that the file is readable
+                if
+                    vim.fn.getbufvar(bufnr, "&modifiable") == 1
+                    and vim.fn.filereadable(vim.api.nvim_buf_get_name(bufnr))
+                then
+                    -- Run the `write` cmd for the specific buffer to save
+                    vim.api.nvim_buf_call(bufnr, function()
+                        vim.cmd("silent! write")
+                    end)
+                end
+            end,
+        })
+    end,
+})
+
+local devicons = require("nvim-web-devicons")
+
+-- When we open a buffer, get icon corresponding to the filetype, and
+-- set the titlestring accordingly.
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+    group = vim.api.nvim_create_augroup("SetTitleString", { clear = true }),
+    callback = function(args)
+        local buf = args.buf
+        local name = vim.api.nvim_buf_get_name(buf)
+        local basename = vim.fs.basename(name)
+        local filetype = vim.fn.getbufvar(buf, "&filetype")
+        local icon = devicons.get_icon(basename, filetype, { default = true })
+        vim.opt.titlestring = icon .. " %t%m"
+    end,
 })
