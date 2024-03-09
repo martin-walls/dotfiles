6cf49f1711f44fc03ed6800ef254866dc76f1d40
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2024-03-09 12:55:03 +0000

    v2024: gitsigns

--- nvim-2024.config/lua/plugins/git.lua
+++ nvim-2024.config/lua/plugins/git.lua
@@ -1,11 +1,17 @@
 return {
-    "tpope/vim-fugitive",
-    config = function()
-        vim.keymap.set("n", "<leader>gs", function()
-            vim.cmd("Git")
-        end, {desc = "[G]it [S]tatus"})
-        vim.keymap.set("n", "<leader>gp", function()
-            vim.cmd("Git push")
-        end, {desc = "[G]it [P]ush"})
-    end,
+    {
+        "tpope/vim-fugitive",
+        config = function()
+            vim.keymap.set("n", "<leader>gs", function()
+                vim.cmd("Git")
+            end, { desc = "[G]it [S]tatus" })
+            vim.keymap.set("n", "<leader>gp", function()
+                vim.cmd("Git push")
+            end, { desc = "[G]it [P]ush" })
+        end,
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        opts = {},
+    },
 }
