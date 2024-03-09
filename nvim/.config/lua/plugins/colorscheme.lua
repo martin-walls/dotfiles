d5e0836703a9f9d7898b6302487ca0d2503388e2
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2024-03-09 16:33:49 +0000

    format

--- nvim/.config/lua/plugins/colorscheme.lua
+++ nvim/.config/lua/plugins/colorscheme.lua
@@ -4,10 +4,12 @@ return {
     lazy = false,
     -- Load this before other plugins
     priority = 1000,
-    config = function ()
+    config = function()
         require("onedark").setup({
-            style = "dark"
+            -- Don't need to change this for light/dark theme, it happens automatically
+            -- when vim.o.background is changed
+            style = "dark",
         })
         require("onedark").load()
-    end
+    end,
 }
