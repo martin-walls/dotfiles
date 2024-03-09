b754a6654bb295a7583cdcf57ee90e552a594fe1
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2024-03-09 13:26:52 +0000

    v2024: vim-tmux-navigator

--- nvim-2024.config/lua/mrw/keymaps.lua
+++ nvim-2024.config/lua/mrw/keymaps.lua
@@ -6,10 +6,11 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
 -- Move between windows
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
+-- NOTE: these are no longer needed when using vim-tmux-navigator
+-- vim.keymap.set("n", "<C-h>", "<C-w>h")
+-- vim.keymap.set("n", "<C-l>", "<C-w>l")
+-- vim.keymap.set("n", "<C-j>", "<C-w>j")
+-- vim.keymap.set("n", "<C-k>", "<C-w>k")
 
 -- Half-page jumps
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
