b8c8b2449ff8f7b7675520aab0de20e61e7d82f7
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2024-03-10 19:07:39 +0000

    safeRequire

--- nvim/.config/lua/mrw/init.lua
+++ nvim/.config/lua/mrw/init.lua
@@ -5,8 +5,17 @@ vim.g.loaded_netrwPlugin = 1
 -- Use <Space> as the leader key
 vim.g.mapleader = " "
 
-require("mrw.lazy")
-require("mrw.opts")
-require("mrw.keymaps")
-require("mrw.filetypes")
-require("mrw.autocmds")
+-- Load a module, and don't stop execution if it throws an error.
+local function safeRequire(module)
+    local success, loadedModule = pcall(require, module)
+    if success then
+        return loadedModule
+    end
+    vim.cmd.echo("Error loading " .. module)
+end
+
+safeRequire("mrw.lazy")
+safeRequire("mrw.opts")
+safeRequire("mrw.keymaps")
+safeRequire("mrw.filetypes")
+safeRequire("mrw.autocmds")
