bacc14e1d56578aad94540ae3b6961d1a3659b68
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2024-03-11 11:48:38 +0000

    devicons typst icon

--- nvim/.config/lua/plugins/devicons.lua
+++ nvim/.config/lua/plugins/devicons.lua
@@ -1,3 +1,8 @@
+local typst_icon = {
+    icon = "",
+    color = "#239dad",
+}
+
 return {
     "nvim-tree/nvim-web-devicons",
     opts = {
@@ -5,6 +10,10 @@ return {
             tex = {
                 icon = "",
             },
+            typst = typst_icon,
+        },
+        override_by_extension = {
+            typ = typst_icon,
         },
     },
 }
