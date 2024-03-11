b9ea608f5cd44a4461a16a801ec9294247994421
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2024-03-11 14:26:49 +0000

    use nvim-autopairs

--- nvim/.config/lua/plugins/mini.lua
+++ nvim/.config/lua/plugins/mini.lua
@@ -1,9 +1,6 @@
 return {
     "echasnovski/mini.nvim",
     config = function ()
-        -- autopairs
-        require("mini.pairs").setup()
-
        -- better around/inside textobjects
         require("mini.ai").setup()
     end,
