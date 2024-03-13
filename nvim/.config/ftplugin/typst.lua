152fe1b78c4e25eaba94d530ab65e88aea0da54d
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2024-03-13 16:05:59 +0000

    fix nvim-surround mapping <leader>S

--- nvim/.config/ftplugin/typst.lua
+++ nvim/.config/ftplugin/typst.lua
@@ -6,3 +6,6 @@ utils.indent_width(2)
 
 -- treat "-" as part of word characters
 vim.opt_local.iskeyword:append("-")
+
+-- TODO: map <CR> to function that detects if the current line is a list item;
+-- if so, create a new list item on the next line, with the same level of indentation
--- nvim/.config/lua/plugins/surround.lua
+++ nvim/.config/lua/plugins/surround.lua
@@ -1,12 +1,16 @@
 return {
     "kylechui/nvim-surround",
-    event = "VeryLazy",
     opts = {
         keymaps = {
             normal = "<leader>s",
+            -- Surround the entire current line
             normal_cur = "<leader>ss",
+            -- Add the surrounding delimiters on a new line above/below
             normal_line = "<leader>S",
+            -- Surround the entire current line with delimiters also on new lines
+            normal_cur_line = "<leader>SS",
             visual = "<leader>s",
+            -- Surround the entire current line
             visual_line = "<leader>S",
         },
     },
