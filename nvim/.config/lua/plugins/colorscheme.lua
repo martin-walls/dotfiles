9f8152f2ac81d3e5db2318557eae32af99c52ea3
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2024-03-14 11:14:38 +0000

    rainbow-delimiters.nvim

--- nvim/.config/lua/plugins/colorscheme.lua
+++ nvim/.config/lua/plugins/colorscheme.lua
@@ -1,23 +1,41 @@
 return {
-    "navarasu/onedark.nvim",
-    -- Make sure to load colorscheme during startup
-    lazy = false,
-    -- Load this before other plugins
-    priority = 1000,
-    config = function()
-        require("onedark").setup({
-            -- Don't need to change this for light/dark theme, it happens automatically
-            -- when vim.o.background is changed
-            style = "dark",
-            -- This line is technically unnecessary when overriding the EndOfBuffer
-            -- highlight group directly
-            ending_tildes = true,
-            highlights = {
-                EndOfBuffer = {
-                    fg = "$grey",
+    {
+        "navarasu/onedark.nvim",
+        -- Make sure to load colorscheme during startup
+        lazy = false,
+        -- Load this before other plugins
+        priority = 1000,
+        config = function()
+            require("onedark").setup({
+                -- Don't need to change this for light/dark theme, it happens automatically
+                -- when vim.o.background is changed
+                style = "dark",
+                -- This line is technically unnecessary when overriding the EndOfBuffer
+                -- highlight group directly
+                ending_tildes = true,
+                highlights = {
+                    EndOfBuffer = {
+                        fg = "$grey",
+                    },
                 },
-            },
-        })
-        require("onedark").load()
-    end,
+            })
+            require("onedark").load()
+        end,
+    },
+    {
+        "HiPhish/rainbow-delimiters.nvim",
+        config = function()
+            vim.g.rainbow_delimiters = {
+                highlight = {
+                    -- TODO: link to onedark colours
+                    "RainbowDelimiterYellow",
+                    "RainbowDelimiterBlue",
+                    "RainbowDelimiterOrange",
+                    "RainbowDelimiterViolet",
+                    "RainbowDelimiterCyan",
+                    "RainbowDelimiterRed",
+                },
+            }
+        end,
+    },
 }
